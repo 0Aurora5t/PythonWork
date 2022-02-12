b8c24acc00fe22cc1a43f6185444ec4226ad8fc2
JiangHuaLi <jhl275936>
2022-02-12 23:10:30 +0800

    获取网页中首页的图片

--- coding/FirstDemo.py
+++ coding/FirstDemo.py
@@ -1,5 +1,5 @@
 '''导入urllib库的URLopen'''
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup as bf
 '''
 #发出请求，获取HTML
@@ -15,8 +15,13 @@ obj=bf(html.read(),'html.parser')
 #从标签head、title里提取标题
 title=obj.head.title
 #使用find_all函数获取所有图片信息
-pic_info=obj.find_all('img')
-#分别打印每张图片的信息
+pic_info=obj.find_all('img',class_="index-logo-src")
+#只提取logo图片的链接
+logo_url="https:"+pic_info[0]['src']
+#使用urlretrieve下载图片
+urlretrieve(logo_url,'logo.png')
+print(logo_url)
+'''#分别打印每张图片的信息
 for i in pic_info:
     print(i)
-print(title)
+print(title)'''
